9a6bc528b84c2041f73473d8dad05394ee10e530
KennethLG <orsonleiziaga@gmail.com>
2020-12-11 22:21:22 -0500

    firebase with firestore

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,29 +10,50 @@ admin.initializeApp({
 	databaseURL: "https://node-firebase-9d788-default-rtdb.firebaseio.com/"
 });
 
-const db = admin.database();
+const db = admin.firestore();
 
 router.get("/", async (req, res, next) => {
-	db.ref("chars").once("value", (snapshot) => {
-		const data = snapshot.val();
+	try {
+		const query = db.collection("chars");
+		const querySnapshot = await query.get();
+		const data = querySnapshot.docs.map(doc => ({
+			id: doc.id,
+			name: doc.data().name,
+			game: doc.data().game
+		}));
+
 		res.render("index", {
 			chars: data
 		});
-	});
+	} catch(e) {
+		console.error(e);
+		next(e);
+	}
 });
 
 router.post("/new-char", async (req, res, next) => {
-	const newChar = {
-		name: req.body.name,
-		game: req.body.game
-	};
-	db.ref("chars").push(newChar);
-	res.redirect("/");
+	try {
+		const newChar = {
+			name: req.body.name,
+			game: req.body.game
+		};
+		await db.collection("chars").doc().create(newChar);
+		res.redirect("/");
+	} catch(e) {
+		console.error(e);
+		next(e);
+	}
 });
 
-router.get("/delete-contact/:id", async (req, res, next) => {
-	db.ref("chars/" + req.params.id).remove();
-	res.redirect("/");
+router.get("/delete-char/:id", async (req, res, next) => {
+	try {
+		const data = db.collection("chars").doc(req.params.id);
+		await data.delete();
+		res.redirect("/");
+	} catch(e) {
+		console.error(e);
+		next(e);
+	}
 })
 
 module.exports = router;
